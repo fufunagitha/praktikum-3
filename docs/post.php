8b2efee34080a8225c716cda5216a80a9562b891
fufunagitha <167779217+fufunagitha@users.noreply.github.com>
2024-06-19 22:55:40 +0700

    Add files via upload

---
+++ docs/post.php
@@ -0,0 +1,8 @@
+<?php
+$nama = $_POST['nama'];
+echo "nama : $nama <br>";
+$email = $_POST['email'];
+echo "email : $email <br>";
+$komentar = $_POST['komentar'];
+echo "komentar : $komentar <br>";
+?>
